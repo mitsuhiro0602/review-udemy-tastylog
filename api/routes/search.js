50c72439113bad73bbbb78c2e1d92f8a82b1e5c0
mitsuhiro0602 <0hornet0@gmail.com>
2022-09-17 23:35:51 +0900

    feat:検索画面のページングを作成

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,28 +1,45 @@
+const MAX_ITEMS_PER_PAGE = require("../config/application.config").search.MAX_ITEMS_PER_PAGE
 const router = require("express").Router();
 const { MySQLClient, sql } = require("../lib/database/client.js");
-const MAX_ITEMS = 5;
+// const MAX_ITEMS = 5;
 
 router.get("/", async (req, res, next) => {
-	var keyword = req.query.keyword || "";
-	var results;
+	let page = req.query.page ? parseInt(req.query.page) : 1;
+	let keyword = req.query.keyword || "";
+	let count, results;
 
 	try {
 		if (keyword) {
+			count = (await MySQLClient.executeQuery(
+				await sql("COUNT_SHOP_BY_NAME"),
+				[`%${keyword}%`]
+			))[0].count;
+			console.log(keyword)
 			results = await MySQLClient.executeQuery(
 				await sql("SELECT_SHOP_LIST_BY_NAME"),
 				[
 					`%${keyword}%`,
-					MAX_ITEMS
+					(page - 1) * MAX_ITEMS_PER_PAGE, // offset
+					MAX_ITEMS_PER_PAGE               // limit
 				]
 			);
 		} else {
+			count = MAX_ITEMS_PER_PAGE
 			results = await MySQLClient.executeQuery(
 				await sql("SELECT_SHOP_HIGH_SCORE_LIST"),
-				[MAX_ITEMS]
+				[MAX_ITEMS_PER_PAGE]
 			);
 		}
 
-		res.render("./search/list.ejs", { results });
+		res.render("./search/list.ejs", {
+			keyword,
+			count,
+			results,
+			pagenation: {
+				max: Math.ceil(count / MAX_ITEMS_PER_PAGE ),
+				current: page
+			}
+		});
 	} catch (err) {
 		next(err);
 	}
